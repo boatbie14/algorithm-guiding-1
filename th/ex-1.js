6147256ae7ae304043c7ad32d7fc924ce33b922b
Kittiphat <kittiphat.rpp@gmail.com>
2025-04-07 08:54:44 +0700

    feat: calculate  algorithm

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,5 +1,36 @@
+/*
+#
+# Option 1 
+#
+*/
+
 const moveZeroes = function (nums) {
-  //Start coding here
+  let zeroArr = [];
+  let numsArr = [];
+
+  nums.filter((num) => {
+    if (num <= 0) {
+      zeroArr.push(num);
+    } else {
+      numsArr.push(num);
+    }
+  });
+
+  console.log("Zero = " + zeroArr);
+  console.log("Num = " + numsArr);
+
+  for (let i = 0; i < numsArr.length - 1; i++) {
+    for (let j = 0; j < numsArr.length - 1 - i; j++) {
+      if (numsArr[j] > numsArr[j + 1]) {
+        let temp = numsArr[j];
+        numsArr[j] = numsArr[j + 1];
+        numsArr[j + 1] = temp;
+      }
+    }
+  }
+
+  const result = [...numsArr, ...zeroArr];
+  return result;
 };
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,12 +1,63 @@
 const romanToInt = function (s) {
-  //Start coding here
+  const spiltText = s.split("");
+  console.log(spiltText);
+
+  const switchToNumber = spiltText.map((text) => {
+    switch (text) {
+      case "I":
+        return 1;
+      case "V":
+        return 5;
+      case "X":
+        return 10;
+      case "L":
+        return 50;
+      case "C":
+        return 100;
+      case "D":
+        return 500;
+      case "M":
+        return 1000;
+      default:
+        return 0;
+    }
+  });
+
+  console.log(switchToNumber);
+
+  let result = 0;
+
+  for (let i = 0; i < switchToNumber.length; i++) {
+    if (switchToNumber[i] < switchToNumber[i + 1]) {
+      result -= switchToNumber[i];
+    } else {
+      result += switchToNumber[i];
+    }
+  }
+
+  return result;
 };
+
+const result4 = romanToInt("IV"); //4
+console.log("result 4 = " + result4);
+console.log("========");
+
+const result5 = romanToInt("VII"); //7
+console.log("result 5 = " + result5);
+console.log("========");
+
+const result6 = romanToInt("VXX"); //7
+console.log("result 5 = " + result6);
+console.log("========");
 
 const result1 = romanToInt("III"); // 3
 console.log(result1);
+console.log("========");
 
 const result2 = romanToInt("LVIII"); // 58
 console.log(result2);
+console.log("========");
 
 const result3 = romanToInt("MCMXCIV"); //1994
-console.log(result3);
+console.log("result = " + result3);
+console.log("========");
